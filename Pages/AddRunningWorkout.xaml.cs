cbe4b4f7e183015c34ea50c732745643d5282ef3
Christoffer Lundberg <148043316+Obaoba90@users.noreply.github.com>
2025-01-05 11:16:40 +0100

    AddWorkout visable collapse

--- Pages/AddRunningWorkout.xaml.cs
+++ Pages/AddRunningWorkout.xaml.cs
@@ -16,4 +16,29 @@ public partial class AddRunningWorkout : ContentPage
 	{
 		await Navigation.PushAsync(new RunningView());
 	}
+
+	private void RunningFrame_Tapped(object sender, TappedEventArgs e)
+	{
+		FrameStack.IsVisible=false;
+    }
+
+	private void WalkingFrame_Tapped(object sender, TappedEventArgs e)
+	{
+		FrameStack.IsVisible = false;
+	}
+
+	private void Swimming_Tapped(object sender, TappedEventArgs e)
+	{
+		FrameStack.IsVisible = false;
+	}
+
+	private void Cycling_Tapped(object sender, TappedEventArgs e)
+	{
+		FrameStack.IsVisible = false;
+	}
+
+	private void Other_Tapped(object sender, TappedEventArgs e)
+	{
+		FrameStack.IsVisible = false;
+	}
 }
