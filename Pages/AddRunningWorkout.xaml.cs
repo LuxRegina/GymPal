89c97b0d63810c307339e69550c41cf1d9887509
Christoffer Lundberg <148043316+Obaoba90@users.noreply.github.com>
2025-01-06 16:44:45 +0100

    Struktur och kommentarer

--- Pages/AddRunningWorkout.xaml.cs
+++ Pages/AddRunningWorkout.xaml.cs
@@ -20,25 +20,30 @@ public partial class AddRunningWorkout : ContentPage
 	private void RunningFrame_Tapped(object sender, TappedEventArgs e)
 	{
 		FrameStack.IsVisible=false;
+		RunForm.IsVisible=true;
     }
 
 	private void WalkingFrame_Tapped(object sender, TappedEventArgs e)
 	{
 		FrameStack.IsVisible = false;
+		WalkForm.IsVisible=true;
 	}
 
 	private void Swimming_Tapped(object sender, TappedEventArgs e)
 	{
 		FrameStack.IsVisible = false;
+		SwimForm.IsVisible=true;
 	}
 
 	private void Cycling_Tapped(object sender, TappedEventArgs e)
 	{
 		FrameStack.IsVisible = false;
+		CycleForm.IsVisible=true;
 	}
 
 	private void Other_Tapped(object sender, TappedEventArgs e)
 	{
 		FrameStack.IsVisible = false;
+		OtherForm.IsVisible=true;
 	}
 }
