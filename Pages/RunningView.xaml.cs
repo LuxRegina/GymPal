ed2577a74a4c06321f9ef69ee46a558c6692f9ba
Christoffer Lundberg <148043316+Obaoba90@users.noreply.github.com>
2025-01-09 10:54:39 +0100

    Skärma av addRunningView/ Runniview look

--- Pages/RunningView.xaml.cs
+++ Pages/RunningView.xaml.cs
@@ -16,4 +16,34 @@ public partial class RunningView : ContentPage
 	{
 		await Navigation.PushAsync(new AddRunningWorkout());
 	}
+
+	private void RunningFrame_Tapped(object sender, TappedEventArgs e)
+	{
+		FrameStack.IsVisible = false;
+		RunForm.IsVisible = true;
+	}
+
+	private void WalkingFrame_Tapped(object sender, TappedEventArgs e)
+	{
+		FrameStack.IsVisible = false;
+		WalkForm.IsVisible = true;
+	}
+
+	private void Swimming_Tapped(object sender, TappedEventArgs e)
+	{
+		FrameStack.IsVisible = false;
+		SwimForm.IsVisible = true;
+	}
+
+	private void Cycling_Tapped(object sender, TappedEventArgs e)
+	{
+		FrameStack.IsVisible = false;
+		CycleForm.IsVisible = true;
+	}
+
+	private void Other_Tapped(object sender, TappedEventArgs e)
+	{
+		FrameStack.IsVisible = false;
+		OtherForm.IsVisible = true;
+	}
 }
